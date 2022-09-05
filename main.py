3eb5181003f1ffa00d7a4296d3767a89d33e0acb
024x <satyendrabongi@gmail.com>
2022-09-05 22:42:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask,request
+app = Flask(__name__)
+import os
+import mongo as m
+
+
+
+@app.route("/",methods=['GET'])
+def read_root():
+    return {'status':'working'}
+@app.route("/register/",methods=['GET'])
+def register():
+    _id = request.args.get('uid', None)
+    mail = request.args.get('mail', None)
+    pas = request.args.get('pass', None)
+    try:
+        return dict(m.regist(_id,mail,pas))
+    except Exception as e:
+        print(e)
+        return {'status': 'Error!, Contact admin'}
+
+@app.route('/verify/',methods=['GET'])
+def verify():
+    hash = request.args.get('hash', None)
+    #return m.verif(hash)
+    try:
+        return m.verif(hash)
+    except Exception as e:
+        print(e)
+        return {'status': 'Error!, Contact admin'}
+        
+app.run(host='0.0.0.0', port=81)
+
+        
+          
---
+++ mongo.py
@@ -0,0 +1,57 @@
+from pymongo import MongoClient
+from email_validator import validate_email
+import string
+import random
+import smtplib
+
+
+def sendmail(too, hash):
+    gmail_user = 'torrleechs@gmail.com'
+    gmail_password = 'aztdvpeskpaclseo'
+    sent_from = gmail_user
+    to = too
+    subject = 'Mail to verify!'
+    body = f'Link for verification is https://024x.eu.org/verify?hash={hash}'
+    email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+
+
+
+%s
+
+""" % (sent_from, ", ".join(to), subject, body)
+    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp_server.ehlo()
+    smtp_server.login(gmail_user, gmail_password)
+    smtp_server.sendmail(sent_from, to, email_text)
+    smtp_server.close()
+
+
+client = MongoClient(
+    'mongodb+srv://satya:s4tya@cluster0.rrqhs.mongodb.net/utyfky?retryWrites=true&w=majority'
+)
+db = client.Apk
+collection = db.user
+
+
+def regist(id, mail, pas):
+    validate_email(mail).email
+    hash = ''.join(random.choices(string.ascii_letters, k=15))
+    sendmail(mail, hash)
+    collection.insert_one({
+        '_id': id,
+        'email': mail,
+        'pass': pas,
+        'is_verified': False,
+        'hash': hash,
+    })
+    return {'status': 'signup Successful Please check your mail and verify!'}
+
+
+def verif(hash):
+    #collection.findOne({'hash': hash})
+    collection.update_one({'hash': hash}, {'$set': {'is_verified': True}, '$unset':{'hash':hash}}  )
+    return {'status': 'Verified!'}
